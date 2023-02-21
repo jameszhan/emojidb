6ae4a86e379fa609f2403c50995c4ed8552b2b72
James Zhan <zhiqiangzhan@gmail.com>
2023-02-22 01:49:04 +0800

    upgrade emoji to emojidb

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="emojidb",
-    version="0.0.2",
+    version="0.0.3",
     author="James Zhan",
     author_email="zhiqiangzhan@gmail.com",
     description="Emoji DB package",
